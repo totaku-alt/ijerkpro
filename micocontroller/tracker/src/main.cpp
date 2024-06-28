1fded5fbe90a965d013ca73730f0d44aec833ea7
Tobias <tobiasheinen@live.com>
2024-06-28 11:05:53 +0200

    simple wifi POST from ESP

--- micocontroller/tracker/src/main.cpp
+++ micocontroller/tracker/src/main.cpp
@@ -1,10 +1,41 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+String api = "http:/ijerk.pro/api";
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+
+    http.begin(api);
+    http.addHeader("Content-Type", "text/plain");
+
+    int httpCode = http.POST("Message from ESP8266");
+    String payload = http.getString();
+
+    Serial.println(httpCode);
+    Serial.println(payload);
+
+    http.end();
+  } else {
+    Serial.println("WiFi not connected");
+  }
 }
